ccc2bbc12241acd10a61fae284214fe30646dccc
statox <me@statox.fr>
2023-11-08 21:16:39 +0000

    Deploying to gh-pages from @ statox/p5-svelte-template@7dec1c994c21ffb736941798721e9050dccbf1cc 🚀

---
+++ _app/immutable/chunks/paths.7ead3f0e.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1p4hxf8)==null?void 0:s.base)??"/p5-svelte-template";var e;const a=((e=globalThis.__sveltekit_1p4hxf8)==null?void 0:e.assets)??t;export{a,t as b};
